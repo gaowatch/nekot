e5579e04885f43d5072574aff2f8279b49831fce
Gao <1743584285@qq.com>
2026-04-01 18:23:52 +0800

    feat: Add TabItemViewModel
    Add TabItemViewModel for tab management

--- NekoT.Desktop/ViewModels/TabItemViewModel.cs
+++ NekoT.Desktop/ViewModels/TabItemViewModel.cs
@@ -18,63 +18,16 @@ public class TabItemViewModel : ViewModelBase
     public event EventHandler? Closed;
     public event EventHandler? Selected;
 
-    public string Title
-    {
-        get => _title;
-        set => SetField(ref _title, value);
-    }
-
-    public bool IsSelected
-    {
-        get => _isSelected;
-        set
-        {
-            if (SetField(ref _isSelected, value) && value)
-            {
-                Selected?.Invoke(this, EventArgs.Empty);
-            }
-        }
-    }
-
-    public void SetIsSelectedSilent(bool value)
-    {
-        if (_isSelected != value)
-        {
-            _isSelected = value;
-            OnPropertyChanged(nameof(IsSelected));
-        }
-    }
-
-    public UserControl? Content
-    {
-        get => _content;
-        set => SetField(ref _content, value);
-    }
-
-    public bool CanClose
-    {
-        get => _canClose;
-        set => SetField(ref _canClose, value);
-    }
-
+    public string Title { get => _title; set => SetField(ref _title, value); }
+    public bool IsSelected { get => _isSelected; set { if (SetField(ref _isSelected, value) && value) Selected?.Invoke(this, EventArgs.Empty); } }
+    public void SetIsSelectedSilent(bool value) { if (_isSelected != value) { _isSelected = value; OnPropertyChanged(nameof(IsSelected)); } }
+    public UserControl? Content { get => _content; set => SetField(ref _content, value); }
+    public bool CanClose { get => _canClose; set => SetField(ref _canClose, value); }
     public string TabType { get; set; } = "browser";
-
     public ICommand Close { get; }
     public ICommand Select { get; }
 
-    public TabItemViewModel()
-    {
-        Close = new RelayCommand(_ => OnClose());
-        Select = new RelayCommand(_ => OnSelect());
-    }
-
-    private void OnClose()
-    {
-        Closed?.Invoke(this, EventArgs.Empty);
-    }
-
-    private void OnSelect()
-    {
-        IsSelected = true;
-    }
+    public TabItemViewModel() { Close = new RelayCommand(_ => OnClose()); Select = new RelayCommand(_ => OnSelect()); }
+    private void OnClose() { Closed?.Invoke(this, EventArgs.Empty); }
+    private void OnSelect() { IsSelected = true; }
 }
