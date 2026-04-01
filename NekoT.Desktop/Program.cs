4ddefe56fb68425b98f4871951758c87e29ec7a2
Gao <1743584285@qq.com>
2026-04-01 19:40:42 +0800

    refactor: 优化 App.axaml.cs 和 Program.cs

--- NekoT.Desktop/Program.cs
+++ NekoT.Desktop/Program.cs
@@ -27,7 +27,6 @@ class Program
 
         if (args.Contains("--restart"))
         {
-            System.Diagnostics.Debug.WriteLine("[Program] Restart mode detected, waiting for old instance to exit...");
             Thread.Sleep(2000);
         }
 
@@ -56,7 +55,6 @@ class Program
         {
             try
             {
-                System.Diagnostics.Debug.WriteLine("[Program] Another instance is already running, exiting...");
                 var existingProcess = FindExistingProcess();
                 if (existingProcess != null)
                 {
@@ -69,13 +67,12 @@ class Program
             }
             finally
             {
-                _singleInstanceMutex.Dispose();
+                _singleInstanceMutex?.Dispose();
                 _singleInstanceMutex = null;
             }
             return false;
         }
 
-        System.Diagnostics.Debug.WriteLine("[Program] Single instance acquired");
         return true;
     }
 
