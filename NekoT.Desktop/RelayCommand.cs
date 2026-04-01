6863f65f9e80321739339d9fd7671dbd879afd20
Gao <1743584285@qq.com>
2026-04-01 10:26:31 +0800

    Add ViewModelBase and RelayCommand

--- NekoT.Desktop/RelayCommand.cs
+++ NekoT.Desktop/RelayCommand.cs
@@ -7,13 +7,13 @@ public class RelayCommand : ICommand
     private readonly Action<object?> _execute;
     private readonly Func<object?, bool>? _canExecute;
 
-    public event EventHandler? CanExecuteChanged;
-
     public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
     {
         _execute = execute ?? throw new ArgumentNullException(nameof(execute));
         _canExecute = canExecute;
     }
+
+    public event EventHandler? CanExecuteChanged;
 
     public bool CanExecute(object? parameter) => _canExecute == null || _canExecute(parameter);
 
