11d483806425132414e99142a038499cf3bcca11
Gao <1743584285@qq.com>
2026-04-01 12:39:33 +0800

    Add language constants and WebView2 service extensions

--- NekoT.Desktop/Constants/LanguageConstants.cs
+++ NekoT.Desktop/Constants/LanguageConstants.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace NekoT.Desktop.Constants;
 
 public static class LanguageConstants
@@ -5,14 +7,6 @@ public static class LanguageConstants
     public const string Chinese = "zh-CN";
     public const string English = "en";
     public static readonly string[] SupportedLanguages = { Chinese, English };
-    
-    public static bool IsValidLanguage(string language) =>
-        Array.Exists(SupportedLanguages, lang => lang == language);
-    
-    public static string GetDisplayName(string language) => language switch
-    {
-        Chinese => "简体中文",
-        English => "English",
-        _ => language
-    };
+    public static bool IsValidLanguage(string language) => Array.Exists(SupportedLanguages, lang => lang == language);
+    public static string GetDisplayName(string language) => language switch { Chinese => "简体中文", English => "English", _ => language };
 }
