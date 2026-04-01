d9303a3238295464ab7189bd2c8f8be19bec2f1a
Gao <1743584285@qq.com>
2026-04-01 12:49:35 +0800

    Add VersionInfo models

--- NekoT.Models/Versioning/VersionInfo.cs
+++ NekoT.Models/Versioning/VersionInfo.cs
@@ -1,10 +1,25 @@
+using System;
+
 namespace NekoT.Models.Versioning;
+
+public enum UpdateType { Optional = 0, Recommended = 1, Forced = 2 }
 
 public class VersionInfo
 {
-    public string Version { get; set; } = string.Empty;
-    public string DownloadUrl { get; set; } = string.Empty;
-    public string ReleaseNotes { get; set; } = string.Empty;
+    public string Version { get; set; } = "0.1.0";
     public DateTime ReleaseDate { get; set; }
+    public string? ReleaseNotes { get; set; }
+    public UpdateType UpdateType { get; set; } = UpdateType.Optional;
+    public string? MinVersion { get; set; }
+    public string? ForceUpdateFromVersion { get; set; }
+    public string? GitHubReleaseUrl { get; set; }
+}
+
+public class UpdateCheckResult
+{
+    public bool HasUpdate { get; set; }
+    public string CurrentVersion { get; set; } = string.Empty;
+    public VersionInfo? LatestVersion { get; set; }
     public bool IsForceUpdate { get; set; }
+    public string? UpdateMessage { get; set; }
 }
