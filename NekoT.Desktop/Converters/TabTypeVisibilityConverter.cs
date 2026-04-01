f424bcd240a6dc310210ab5d91756c251b51d306
Gao <1743584285@qq.com>
2026-04-01 12:32:38 +0800

    Add Desktop converters for UI bindings

--- NekoT.Desktop/Converters/TabTypeVisibilityConverter.cs
+++ NekoT.Desktop/Converters/TabTypeVisibilityConverter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using Avalonia.Data.Converters;
 
@@ -6,24 +7,12 @@ namespace NekoT.Desktop.Converters;
 
 public class TabTypeVisibilityConverter : IValueConverter
 {
-    private static readonly System.Collections.Generic.HashSet<string> HiddenTabTypes = new(StringComparer.OrdinalIgnoreCase)
-    {
-        "home",
-        "chat"
-    };
+    private static readonly HashSet<string> HiddenTabTypes = new(StringComparer.OrdinalIgnoreCase) { "home", "chat" };
 
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is not string tabType || string.IsNullOrWhiteSpace(tabType))
-        {
-            return true;
-        }
-
+        if (value is not string tabType || string.IsNullOrWhiteSpace(tabType)) return true;
         return !HiddenTabTypes.Contains(tabType);
     }
-
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
-    }
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotImplementedException();
 }
